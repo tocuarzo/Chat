bcf74df53b019f7d29fb12d41265337fc2c3213f
iZAC <olivator91@gmail.com>
2018-10-11 14:16:11 +0200

    V0.4

--- src/Controller/indexController.php
+++ src/Controller/indexController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Mensaje;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -25,8 +26,19 @@ class indexController extends AbstractController
      */
     public function main(SessionInterface $session){
         $user = $session->get("user");
+        $times = $session->get("login_times");
         if (is_null($user)){
             return $this->redirectToRoute("login");
+        }
+        if (!$times){
+            $em = $this->getDoctrine()->getManager();
+            $loginmsg = new Mensaje();
+            $loginmsg->setUsuario("Admin");
+            $loginmsg->setMensaje("$user entra en la sesion");
+            $em->persist($loginmsg);
+            $em->flush();
+            $times = true;
+            $session->set("login_times", $times);
         }
         return $this->render("main.html.twig", array("user" => $user));
     }
@@ -39,6 +51,7 @@ class indexController extends AbstractController
         $userform = $request->get("user");
         if (!empty($userform) && !is_null($userform)){
             $session->set("user", $userform);
+            $session->set("login_times", false);
             return $this->redirectToRoute("main");
         } else {
             return $this->render("login.html.twig");
@@ -48,6 +61,13 @@ class indexController extends AbstractController
      * @Route("/logout", name="logout", methods={"GET"})
      */
     public function logout(SessionInterface $session){
+        $user = $session->get("user");
+        $em = $this->getDoctrine()->getManager();
+        $logoutmsg = new Mensaje();
+        $logoutmsg->setUsuario("Admin");
+        $logoutmsg->setMensaje("$user abandona la sesion");
+        $em->persist($logoutmsg);
+        $em->flush();
         $session->clear();
         return $this->redirectToRoute("index");
     }
